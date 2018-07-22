b6b05cdaa4a58ca136b37fbe4ac96a2b89c9ba46
Kay B <kaylouisebennett@gmail.com>
2018-07-22 11:56:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
 
-var theFunk = funkyFunction('FUNKY!')
+var theFunk = funkyFunction(function)
 
 theFunk();
 
